191f3b7d41764d98f843f1ed5a273d33ea4d7842
olegL20 <oleglituga@gmail.com>
2017-05-04 21:38:47 +0300

    Passport конфигурация

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('error');
+/* GET register page */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
