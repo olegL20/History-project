ca7e4939d16b6cc59143d5f80ab3271980a884d4
olegL20 <oleglituga@gmail.com>
2017-05-12 05:12:24 +0300

    Регистрация+ Авторизация-

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/user');
 /* GET register page */
 router.get('/register', function(req, res, next) {
     res.render('register');
@@ -9,4 +9,48 @@ router.get('/login', function(req, res, next) {
     res.render('login');
 });
 
+router.post('/register', function(req, res, next) {
+    var name = req.body.name;
+    var surname = req.body.surname;
+    var email = req.body.email;
+    var pass1 = req.body.password;
+    var pass2 = req.body.password2;
+    var teacher = req.body.teacher;
+//validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('surname', 'Surname is required').notEmpty();
+    req.checkBody('teacher', 'Teacher is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {errors: errors});
+        console.log(errors)
+    }
+    else {
+        console.log(name,surname,email, pass1,teacher);
+
+        var newUser = new User(
+            {
+                name: name,
+                surname: surname,
+                email: email,
+                password: pass1,
+                teacher: teacher
+            });
+
+        User.createUser(newUser, function (err, user) {
+
+            console.log(user);
+        });
+
+    }
+
+
+});
 module.exports = router;
